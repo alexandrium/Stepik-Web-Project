e56a989fbdb5fec5c48b686e582e249347a84703
Alex <printscreen777@yandex.ru>
2020-06-30 19:01:56 +0300

    Updated

--- etc/g_conf.py
+++ etc/g_conf.py
@@ -2,4 +2,4 @@ bind = "0.0.0.0:8080"
 workers = 1
 # module = hello:app
 # gunicorn -b 0.0.0.0:8080 --workers=1 hello:app
-
+# gunicorn -c CONFIG hello:app
--- etc/hello.py
+++ etc/hello.py
@@ -1,20 +1,10 @@
 def app(environ, start_response):
-    """Simplest possible application object"""
-
-#    try:
-#        request_body_size = int(environ.get('CONTENT_LENGTH',0))
-#    except (ValueError):
-#        request_body_size = 0
-
-    # data = bytearray(request_body_size) 
-    # data = str(environ['wsgi.input']).encode()
-    
-#    request_body = environ['wsgi.input'].read(request_body_size)
-#    data = request_body
 
     data = environ['QUERY_STRING'].encode()    
-#    data = data.split(b'&')
-    data = data.replace(b'&',b'\n')
+
+   # data = data.split(b'&')
+
+    data = data.replace(b'&', b'\n')
 
     status = '200 OK'
     response_headers = [
@@ -23,5 +13,6 @@ def app(environ, start_response):
     ]
     start_response(status, response_headers)
     return iter([data])
+
 #    return [data]
 #    return iter(data)
