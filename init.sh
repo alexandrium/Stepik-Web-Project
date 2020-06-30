6adbffd7dcfa8af244ede041639494a674b153e7
Alex <printscreen777@yandex.ru>
2020-06-30 21:20:03 +0300

    Let's go

--- init.sh
+++ init.sh
@@ -11,4 +11,10 @@ sudo pip3 install --upgrade pip
 # sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
 
+
+
+### 1.9 Архитектура frontend-backend
+
 sudo ln -fs /etc/gunicorn.d/hello.py /home/box/web/etc/g_conf.py
+# cd /home/box/web
+# python3.4 /usr/local/bin/gunicorn -b 0.0.0.0:8080 hello:app
