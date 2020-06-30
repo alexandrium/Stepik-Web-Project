b2bb7f2390a600c545d7d4bb11494db819622365
Alex <printscreen777@yandex.ru>
2020-06-30 20:44:18 +0300

    Let's go

---
+++ init.sh
@@ -0,0 +1,14 @@
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+
+sudo apt-get update
+sudo apt-get install -y python3.5
+# sudo apt-get install -y python3.5-dev
+# sudo unlink /usr/bin/python3
+sudo ln -fs /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+# sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
+
+sudo ln -fs /etc/gunicorn.d/hello.py /home/box/web/etc/g_conf.py
