24948acf7c5cb1c89c7c92449020df25ad6f5df7
Alex <printscreen777@yandex.ru>
2020-07-18 20:56:57 +0300

    signup, login, logout....

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
 
     path('', views.new, name='new'),
     # path('login/', views.login, name='login'),
-    path('login', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user='True'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user='True'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', views.signup, name='password_reset'),
     # path('signup/', views.signup, name='signup'),
 
